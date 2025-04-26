77438b2e951b292a853885a9cd04b0c91bc57717
Joel Estiven Orozco Hernández <157641638+JowelBB@users.noreply.github.com>
2025-04-26 11:51:06 -0500

    Updating multiple files

--- 5TO/ZISVA-Electiva Disciplinar II 25-1/Actividad_4/main.py
+++ 5TO/ZISVA-Electiva Disciplinar II 25-1/Actividad_4/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from db_config import SessionLocal, engine
-from models import Base, Item
-from crud import get_item, create_item, get_all_items, update_item, delete_item
+from models import Base, Products
+from recursos.crud_productos import get_product, get_all_products, create_product, update_product, delete_product
+from recursos.crud_categorias import get_category, get_all_categories, create_category, update_category, delete_category
 
 app = FastAPI()
 
@@ -17,33 +18,64 @@ def get_db():
     finally:
         db.close()
 
+# Rutes for products
 @app.post("/productos/")
-async def get_item_endpoint(nombre: str, descripcion: str, precio: int, categoria_id: int, db: Session = Depends(get_db)):
-    return create_item(db, nombre, descripcion, precio, categoria_id)
+async def get_item_endpoint(name: str, description: str, price: int, categoria_id: int, db: Session = Depends(get_db)):
+    return create_product(db, name, description, price, categoria_id)
 
 @app.get("/productos/")
 async def get_all_items_endpoint(db: Session = Depends(get_db)):
-    items = get_all_items(db)
-    return items
+    products = get_all_products(db)
+    return products
 
-@app.get("/productos/{item_id}")
-async def get_item_endpoint(item_id: int, db: Session = Depends(get_db)):
-    item = get_item(db, item_id)
-    if item is None:
-        raise HTTPException(status_code=404, detail="Product not fount")
-    return item
+@app.get("/productos/{product_id}")
+async def get_item_endpoint(product_id: int, db: Session = Depends(get_db)):
+    product = get_product(db, product_id)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
 
-@app.put("/productos/{item_id}")
-async def update_item_endpoint(item_id: int, nombre: str = None, descripcion: str = None, precio: int = None, categoria_id: int = None, db: Session = Depends(get_db)):
-    updated_item = update_item(db, item_id, nombre, descripcion, precio, categoria_id)
-    if updated_item is None:
-        raise HTTPException(status_code=404, detail="Producto no encontrado")
-    return updated_item
+@app.put("/productos/{product_id}")
+async def update_item_endpoint(product_id: int, name: str = None, description: str = None, price: int = None, categoria_id: int = None, db: Session = Depends(get_db)):
+    updated_product = update_product(db, product_id, name, description, price, categoria_id)
+    if updated_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return updated_product
 
-@app.delete("/productos/{item_id}")
-async def delete_item_endpoint(item_id: int, db: Session = Depends(get_db)):
-    if delete_item(db, item_id):
-        return {"detail": "Producto eliminado"}
+@app.delete("/productos/{product_id}")
+async def delete_item_endpoint(product_id: int, db: Session = Depends(get_db)):
+    if delete_product(db, product_id):
+        return {"detail": "Deleted product"}
     else:
-        raise HTTPException(status_code=404, detail="Product not fount")
+        raise HTTPException(status_code=404, detail="Product not found")
 
+# Rutes for Categories
+@app.post("/categorias/")
+async def get_category_endpoint(name: str, db: Session = Depends(get_db)):
+    return create_category(db, name)
+
+@app.get("/categorias/")
+async def get_all_categories_endpoint(db: Session = Depends(get_db)):
+    categories = get_all_categories(db)
+    return categories
+
+@app.get("/categorias/{category_id}")
+async def get_category_endpoint(category_id: int, db: Session = Depends(get_db)):
+    category = get_category(db, category_id)
+    if category is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return category
+
+@app.put("/categorias/{category_id}")
+async def update_category_endpoint(category_id: int, name: str = None, db: Session = Depends(get_db)):
+    updated_category = update_category(db, category_id, name)
+    if updated_category is None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return updated_category
+
+@app.delete("/categorias/{category_id}")
+async def delete_category_endpoint(category_id: int, db: Session = Depends(get_db)):
+    if delete_category(db, category_id):
+        return {"detail": "Deleted category"}
+    else:
+        raise HTTPException(status_code=404, detail="Product not found")
--- 5TO/ZISVA-Electiva Disciplinar II 25-1/Actividad_4/models.py
+++ 5TO/ZISVA-Electiva Disciplinar II 25-1/Actividad_4/models.py
@@ -1,10 +1,15 @@
 from sqlalchemy import Column, Integer, String
 from db_config import Base
 
-class Item(Base):
+class Products(Base):
     __tablename__ = "productos"
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String(255), index=True)
     descripcion = Column(String(255), index=True)
     precio = Column(Integer)
     categoria_id = Column(Integer)
+
+class Categories(Base):
+    __tablename__ = "categorias"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(255), index=True)

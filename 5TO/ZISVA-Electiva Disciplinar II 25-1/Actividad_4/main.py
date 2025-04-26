4401a138e36785a58f6ce657b0cbd2a80c21a956
Joel Estiven Orozco Hernández <157641638+JowelBB@users.noreply.github.com>
2025-04-26 09:26:31 -0500

    Update crud  and main file

--- 5TO/ZISVA-Electiva Disciplinar II 25-1/Actividad_4/main.py
+++ 5TO/ZISVA-Electiva Disciplinar II 25-1/Actividad_4/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from db_config import SessionLocal, engine
 from models import Base, Item
-from crud import get_item, create_item
+from crud import get_item, create_item, get_all_items, update_item, delete_item
 
 app = FastAPI()
 
@@ -21,9 +21,29 @@ def get_db():
 async def get_item_endpoint(nombre: str, descripcion: str, precio: int, categoria_id: int, db: Session = Depends(get_db)):
     return create_item(db, nombre, descripcion, precio, categoria_id)
 
+@app.get("/productos/")
+async def get_all_items_endpoint(db: Session = Depends(get_db)):
+    items = get_all_items(db)
+    return items
+
 @app.get("/productos/{item_id}")
 async def get_item_endpoint(item_id: int, db: Session = Depends(get_db)):
     item = get_item(db, item_id)
     if item is None:
-        raise HTTPException(status_code=404, detail="Item not fount")
+        raise HTTPException(status_code=404, detail="Product not fount")
     return item
+
+@app.put("/productos/{item_id}")
+async def update_item_endpoint(item_id: int, nombre: str = None, descripcion: str = None, precio: int = None, categoria_id: int = None, db: Session = Depends(get_db)):
+    updated_item = update_item(db, item_id, nombre, descripcion, precio, categoria_id)
+    if updated_item is None:
+        raise HTTPException(status_code=404, detail="Producto no encontrado")
+    return updated_item
+
+@app.delete("/productos/{item_id}")
+async def delete_item_endpoint(item_id: int, db: Session = Depends(get_db)):
+    if delete_item(db, item_id):
+        return {"detail": "Producto eliminado"}
+    else:
+        raise HTTPException(status_code=404, detail="Product not fount")
+
